4c818b75f0de9a299b9864debc97d295c54d2042
joan.tolos <joan.tolos@clarivate.com>
2017-11-28 17:36:30 +0100

    Minor refactor

--- src/main/java/com/joantolos/kata/social/networking/core/Clock.java
+++ src/main/java/com/joantolos/kata/social/networking/core/Clock.java
@@ -10,9 +10,13 @@ public class Clock {
         Long millisecondsIncrement = date.getTime() - new Timestamp(System.currentTimeMillis()).getTime();
 
         return new TimeLapse(
-                (millisecondsIncrement / (24 * 60 * 60 * 1000)) * -1,
-                (millisecondsIncrement / (60 * 60 * 1000)) * -1,
-                (millisecondsIncrement / (60 * 1000)) * -1,
-                (millisecondsIncrement / 1000) * -1);
+                getIncrement(millisecondsIncrement, 24 * 60 * 60 * 1000),
+                getIncrement(millisecondsIncrement, 60 * 60 * 1000),
+                getIncrement(millisecondsIncrement, 60 * 1000),
+                getIncrement(millisecondsIncrement, 1000));
+    }
+
+    private long getIncrement(Long millisecondsIncrement, Integer factor) {
+        return (millisecondsIncrement / factor) * -1;
     }
 }
